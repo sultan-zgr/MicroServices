3a6815b2e86343844b342b9388b82fea0e797f93
sultan-zgr <88083356+sultan-zgr@users.noreply.github.com>
2023-05-06 23:12:34 +0300

    Add project files.

---
+++ Services/Contact.API/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Contact.API.Infrastructure;
+using Contact.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contact.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService contactService;
+
+        public ContactController(IContactService ContactService)
+        {
+            contactService = ContactService;
+        }
+        [HttpGet("{id}")]
+        public ContactDTO Get(int id) 
+        {
+            return contactService.GetContactById(id);
+        }
+    }
+}
---
+++ Services/Reservation.API/Reservation.API/Controllers/RestController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Infrastructure;
+using Reservation.API.Models;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestController : ControllerBase
+    {
+        private readonly IReservationService reservationService;
+
+        public RestController(IReservationService ReservationService)
+        {
+            reservationService = ReservationService;
+        }
+        [HttpGet("{Id}")]
+        public ReservationDTO Get(int id) 
+        {
+            return reservationService.GetResByBkgNumber(id);
+        }
+    }
+}
